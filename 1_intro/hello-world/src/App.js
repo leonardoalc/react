38a83e8025fb524ad99f9b3efe2181bdcdecb32a
LeL-nardo <sandroleonardoads@gmail.com>
2022-11-07 18:02:05 -0300

    iniciando react

---
+++ 1_intro/hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <h2>Emmet Funcionou</h2>
+    </div>
+  );
+}
+
+export default App;

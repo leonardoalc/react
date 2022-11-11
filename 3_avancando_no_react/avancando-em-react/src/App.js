37170e4942613b4592ecf1cb7fcf6b7940d06d43
LeL-nardo <sandroleonardoads@gmail.com>
2022-11-11 17:51:41 -0300

    fundamentos +

--- 3_avancando_no_react/avancando-em-react/src/App.js
+++ 3_avancando_no_react/avancando-em-react/src/App.js
@@ -3,9 +3,16 @@ import ManageData from './components/ManageData';
 import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
 import CarDetails from './components/CarDetails';
+import Fragment from './components/Fragment';
 
 // Importando imagens
 import City from "./assets/city.jpg"
+
+const cars = [{id: 1, brand: "KIA", km: 12330, color: "Azul", newCar: false},
+{id: 2, brand: "Ferrari", km: 0, color: "Vermelho", newCar: true},
+{id: 3, brand: "Chevrolet", km: 0, color: "Amarelo", newCar: true}
+]
+
 
 function App() {
   return (
@@ -26,6 +33,17 @@ function App() {
       {/* Reutilizando o car details */}
       <CarDetails brand="Fiat" km={23455} color="Azul" newCar={false}/>
       <CarDetails brand="Renault" km={0} color="Prata" newCar={true}/>
+      {/* Reutilizando o car details mas em loop */}
+      {cars.map((cars) => (
+        <CarDetails 
+        key={cars.id}
+        brand={cars.brand} 
+        km={cars.km} 
+        color={cars.color} 
+        newCar={cars.newCar}
+        />
+      ))}
+      <Fragment/>
     </div>
   );
 }

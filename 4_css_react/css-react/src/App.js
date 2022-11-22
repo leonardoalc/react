2e15997fec3e52ced212d78b6a196d990f7be815
LeL-nardo <sandroleonardoads@gmail.com>
2022-11-22 14:54:01 -0300

    css react ++

--- 4_css_react/css-react/src/App.js
+++ 4_css_react/css-react/src/App.js
@@ -1,6 +1,15 @@
 import './App.css';
+// componentes
 import MyComponent from './components/MyComponent';
+import Title from './components/Title';
+// hooks 
+import { useState } from 'react';
+
+
 function App() {
+  const [n, setN] = useState(11)
+
+  const [redTitle, setredTitle] = useState(true)
   return (
     <div className="App">
       {/* CSS Global */}
@@ -8,6 +17,16 @@ function App() {
       {/* CSS Componente */}
       <MyComponent/>
       <p>Este parágrafo não é de componente</p>
+      {/* CSS INLINE */}
+      <p style={{ color: "blue", padding: "25px", border: "5px solid magenta", borderTop: "9px solid purple"}}>Este está sendo feito com CSS inline</p>
+      {/* CSS INLINE DINÂMICO */}
+      <hr />
+      <h2 style={n < 10 ? ({backgroundColor: "purple"}) : ({backgroundColor: "Cyan"})} id="h2-dinamic">CSS Dinâmico inline</h2>
+      <button onClick={() => (n > 10 ? (setN(9)) : (setN(11)))}>Mudar Cor</button>
+      {/* CSS CLASSES DINÂMICAS */}
+      <h2 className={redTitle ? "red-title" : "title"} onClick={() => redTitle ? (setredTitle(false)) : (setredTitle(true))}>Este título terá classe dinâmica! Clique nele!</h2>
+      {/* CSS MODULES */}
+      <Title/>
     </div>
   );
 }
--- 4_css_react/css-react/src/components/MyComponent.js
+++ 4_css_react/css-react/src/components/MyComponent.js
@@ -5,7 +5,7 @@ const MyComponent = () => {
     <div>
         <h1>Css de Componente</h1>
         <p>Parágrafo de componente.</p>
-        <p className="p-comp">Este também é, mas não está com css vazando</p>
+        <p className="p-my-comp">Este também é, mas não está com css vazando</p>
     </div>
   )
 }

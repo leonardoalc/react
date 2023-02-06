2b90ee663c720260ce4fcbd9c5cb9c3bc5b24b33
LeL-nardo <sandroleonardoads@gmail.com>
2023-02-06 20:29:09 -0300

    reactgram .env e npm

---
+++ 12_reactgram/instagramreact/backennd/app.js
@@ -0,0 +1,29 @@
+const dotenv = require("dotenv").config()
+
+const express = require("express")
+const path = require("path")
+const cors = require("cors")
+
+const port = process.env.PORT
+
+const app = express()
+
+// config JSON an dorm data response
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// Solve CORS
+app.use(cors({credentials:true, origin: "http://localhost:3000"}))
+
+// upload directory
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
+
+// connection db
+const db = require("./config/db")
+
+// routes
+const router = require("./routes/Router")
+
+app.listen(port, () => {
+    console.log('App rodando na porta: ' + port)
+})

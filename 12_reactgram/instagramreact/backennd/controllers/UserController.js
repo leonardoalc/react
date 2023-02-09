20783d30a52af5e63f66122eb0edd14c57f73c45
LeL-nardo <sandroleonardoads@gmail.com>
2023-02-09 15:56:15 -0300

    reactgram autenticação e login

--- 12_reactgram/instagramreact/backennd/controllers/UserController.js
+++ 12_reactgram/instagramreact/backennd/controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User")
+ const User = require("../models/User")
 
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
@@ -11,9 +11,65 @@ const generateToken = (id) => {
 }
 
 const register = async (req, res) => {
-    res.send("Registro")
+    const {name, email, password} = req.body
+
+    // check if user exists
+    const user = await User.findOne({email})
+
+    if(user) {
+        res.status(422).json({errors: ["Por favor, utilize outro email."]})
+        return
+    }
+
+    // Generate password hash
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user 
+    const newUser = await User.create({
+        name,
+        email,
+        password: passwordHash
+    })
+
+    // if user was created sucefully, return token
+    if (!newUser) {
+        res.status(422).json({errors: ["Houve um erro, por favor tente mais tarde."]})
+        return
+    }
+
+    res.status(201).json({
+        _id: newUser._id,
+        token: generateToken(newUser._id)
+    })
+}
+
+// Login route
+const login = async (req, res) => {
+    
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    // check if user exists
+    if(!user) {
+        res.status(422).json({errors: ["Usuário não encotrado"]})
+    }
+
+    // check if password matches
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({errors: ["Senha inválida."]})
+    }
+
+    // return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    })
 }
 
 module.exports = {
     register,
+    login
 }
--- 12_reactgram/instagramreact/backennd/middlewares/userValidations.js
+++ 12_reactgram/instagramreact/backennd/middlewares/userValidations.js
@@ -13,7 +13,7 @@ const userCreateValidation = () => {
             .isEmail()
             .withMessage("Insira um email válido."),
         body("password")
-            .isString("")
+            .isString()
             .withMessage("A senha é obrigatória.")
             .isLength({min: 5})
             .withMessage("A senha precisa ter no mínimo 3 caracteres."),
@@ -29,6 +29,20 @@ const userCreateValidation = () => {
 ]
 }
 
+const loginValidation = () => {
+    return [
+        body("email")
+            .isString()
+            .withMessage("O e-mail é obrigatório.")
+            .isEmail()
+            .withMessage("Insira um email válido."),
+        body("password")
+            .isString()
+            .withMessage("A senha é obrigatória.")
+    ]
+}
+
 module.exports = {
     userCreateValidation,
+    loginValidation
 }

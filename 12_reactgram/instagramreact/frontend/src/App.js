513f3464e328ac5a42e3ef4767e56435ad7cedb9
LeL-nardo <sandroleonardoads@gmail.com>
2023-02-17 12:06:35 -0300

    reactgram frontend + routes e pages

---
+++ 12_reactgram/instagramreact/frontend/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+// router
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom"
+
+// pages
+import Home from './pages/Home/Home';
+import Login from "./pages/Auth/Login"
+import Register  from "./pages/Auth/Register"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ 12_reactgram/instagramreact/frontend/src/pages/Auth/Register.js
@@ -0,0 +1,8 @@
+import "./Auth.css"
+
+const Register = () => {
+  return (
+    <div>Register</div>
+  )
+}
+export default Register

3e6812a02d92f66ba984c30f38e7216050a106af
LeL-nardo <sandroleonardoads@gmail.com>
2023-02-20 18:51:04 -0300

    reactgram css global e form register pt1

--- 12_reactgram/instagramreact/frontend/src/pages/Auth/Register.js
+++ 12_reactgram/instagramreact/frontend/src/pages/Auth/Register.js
@@ -1,8 +1,44 @@
 import "./Auth.css"
 
+// components
+import {Link} from "react-router-dom"
+
+// hooks
+import { useState, useEffect } from "react"
+
 const Register = () => {
+  const [name, setname] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+  const [confirmPassword, setconfirmPassword] = useState("")
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    const user = {
+      name,
+      email,
+      password,
+      confirmPassword
+    }
+
+    console.log(user)
+  }
   return (
-    <div>Register</div>
+    <div id="register">
+      <h2>ReactGram</h2>
+      <p className="subtitle">Cadastre-se para ver as fotos de seus amigos.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Nome" value={name} onChange={e => setname(e.target.value)} autoComplete="name"/>
+        <input type="email" placeholder="E-mail" value={email} onChange={e => setemail(e.target.value)} autoComplete="email"/>
+        <input type="password" placeholder="Senha" value={password} onChange={e => setpassword(e.target.value)} autoComplete="new-password"/>
+        <input type="password" placeholder="Confirme sua senha" value={confirmPassword} onChange={e => setconfirmPassword(e.target.value)} autoComplete="new-password"/>
+
+        <input type="submit" value="Cadastrar"/>
+      </form>
+      <p>Já possui conta? <Link to="/login">Clique aqui.</Link></p>
+    </div>
   )
 }
 export default Register
